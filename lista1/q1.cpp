f275e3fc41a12893132dea817d4bc8dc138aeea6
Maykon Morais <maykon.estudos@gmail.com>
2020-10-31 18:08:04 -0300

    :zap: Questao 1

--- lista1/q1.cpp
+++ lista1/q1.cpp
@@ -1,22 +1,29 @@
 //ref https://leetcode.com/problems/contains-duplicate/
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
+
 using namespace std;
-bool containsDuplicate(vector<int> nums);
+class ContainsDuplicate
+{
+public:
+    static bool containsDuplicate(vector<int>& nums) {
+        if(nums.empty()) {
+            return false;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size()-1; ++i) {
+            if(nums[i] == nums[i+1]) return true;
+        }
+
+        return false;
+    }
+};
 
 int main()
-{
+{  
     vector<int> nums{1, 2, 3, 4, 1};
-    string result = containsDuplicate(nums) ? "true" : "false";
-    cout << "isDuplicated: " << result;
-}
-
-bool containsDuplicate(vector<int> nums)
-{
-    for (int i = 0; i < nums.size(); i++)
-        for (int j = i + 1; j < nums.size(); j++)
-            if (nums[i] == nums[j])
-                return true;
-    return false;
+    cout << "isDuplicated: " << ContainsDuplicate::containsDuplicate(nums) ? true: false;
 }
